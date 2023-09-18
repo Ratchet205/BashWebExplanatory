854244a8e337c529cb3cde1c9bb2d3518a12ebfd
Adrian <adrian@seiboldsmuehle.de>
2023-09-18 23:50:53 +0200

    overscroll fix, start behaviour of nav fix
    fixed overscroll being too big, fixed nav displaying wrong burger icon (x instead of burger)

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,7 @@ function changeIframeSource(url, self) {
         'Split', 'Comparison', 'Multi-line', 'Replacement', 'Substring-Extraction', 'Manipulations-Bibliothek'
     ];
 
-    idList.forEach(function(id) {
+    idList.forEach(function (id) {
         if (id !== self) {
             var element = document.getElementById(id);
             element.style.backgroundColor = '#292929';
@@ -25,13 +25,26 @@ function changeIframeSource(url, self) {
     closeNav();
 }
 
+function closeNav() {
+    var burgerButton = document.getElementById("full-burger");
+    burgerButton.classList.remove('active');
+
+    if (document.body.clientWidth > 600) {
+        document.getElementById("nav").style.width = "0px";
+        document.getElementById("page-content").style.marginLeft = "0px";
+    } else {
+        document.getElementById("nav").style.width = "0px";
+        document.getElementById("page-content").style.marginLeft = "0px";
+    }
+}
+
 function interactNav(burgerButton) {
     var isActive = burgerButton.classList.contains('active');
 
     burgerButton.classList.toggle('active');
 
-    if(document.body.clientWidth > 600){
-        if(!isActive){
+    if (document.body.clientWidth > 600) {
+        if (!isActive) {
             document.getElementById("nav").style.width = "250px";
             document.getElementById("page-content").style.marginLeft = "250px";
         } else {
@@ -39,26 +52,27 @@ function interactNav(burgerButton) {
             document.getElementById("page-content").style.marginLeft = "0px";
         }
     } else {
-        if(!isActive){
+        if (!isActive) {
             document.getElementById("nav").style.width = "100%";
             document.getElementById("page-content").style.marginLeft = "100%";
         } else {
             document.getElementById("nav").style.width = "0px";
             document.getElementById("page-content").style.marginLeft = "0px";
-        } 
+        }
     }
 
 }
 
-function closeNav() {
+function loadNav() {
     var burgerButton = document.getElementById("full-burger");
-    burgerButton.classList.remove('active');
 
-    if(document.body.clientWidth > 600){
-        document.getElementById("nav").style.width = "0px";
-        document.getElementById("page-content").style.marginLeft = "0px";
+    if (document.body.clientWidth > 600) {
+        document.getElementById("nav").style.width = "250px";
+        document.getElementById("page-content").style.marginLeft = "250px";
+        burgerButton.classList.add('active');
     } else {
         document.getElementById("nav").style.width = "0px";
         document.getElementById("page-content").style.marginLeft = "0px";
+        burgerButton.classList.remove('active');
     }
 }
