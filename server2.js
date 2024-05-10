cd75058e46f4bc5b43ff36b255e7d4e2c062e885
Kerling <205ratchet@gmail.com>
2024-05-10 10:43:34 +0200

    cleaning code
    removed unnecessary files, interpreters and old docker code

--- server2.js
+++ server2.js
@@ -1,36 +1,166 @@
-//server
 const express = require('express');
-const app = express();
-const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+const jquery = require('jquery');
+const glob = require('glob');
+const net = require('net');
 const fs = require('fs');
+const { exec } = require('child_process');
+const phpExpress = require('php-express')({
+    binPath: __dirname + '/php-8.2.10',
+  });
+const app = express();
+const server = http.createServer(app);
 
-console.clear();
+const io = socketIO(server);
 
-app.get('/', (req, res) => {
-    const rawIp = req.socket.remoteAddress;
-    const ipv4 = rawIp.replace(/^.*:/, '');
+const pattern = '*.apef';
+// Der Pfad zur ICO-Datei
+const iconFilePath = './public/img/apef.ico';
+// Der Pfad zur erstellten APEF-Datei
+const apefFilePath = './*.apef';
 
-    console.log(`Requesting Client:\nIP:\t${ipv4}\nUser-Agent:\t${req.get('User-Agent')}`);
 
-    // Specify the file path
-    const filePath = path.join(__dirname, 'public', 'index.html');
-    const variable = "hallo"; //here the cookie have to be gained, not in the "js to display" file!!!
-    // Read the contents of the file
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send(err.message);
-            return;
+
+
+glob(pattern, (err, files) => {
+  if (err) {
+    console.error('Error matching files:', err);
+    return;
+  }
+
+  if (files.length === 0) {
+    console.log('No .apef files found.');
+    return;
+  }
+
+  files.forEach((file) => {
+    fs.unlink(file, (err) => {
+      if (err) {
+        console.error(`Error deleting file ${file}:`, err);
+      } else {
+        console.log(`Deleted file: ${file}`);
+      }
+    });
+  });
+});
+
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    if (req.path.endsWith('.php')) {
+        // Set the content type to HTML for PHP files
+        res.contentType('text/html');
+
+        // Execute the PHP script using php-express
+        phpExpress(req, res, next);
+    } else {
+        // Pass control to the next middleware for non-PHP files
+        next();
+    }
+});
+
+io.on('connection', (socket) => {
+    const clientAddress = socket.handshake.address;
+    const family = socket.handshake.headers['x-forwarded-for'] ? 'IPv6' : clientAddress.includes(':') ? 'IPv6' : 'IPv4';
+    const ipAddress = family === 'IPv4' ? clientAddress : clientAddress.split('::ffff:')[1];
+
+    try {
+        //const JsonIP = JSON.parse(ipAddress);
+
+        // Create a JSON file with the received settings asynchronously
+        const filename = `${ipAddress}.apef`;
+
+        if (fs.existsSync(`./${ipAddress}.apef`)) {
+            path = `./${ipAddress}.apef`
+            fs.readFile(path, 'utf8', (err, data) => {
+                if (err) {
+                    console.error(`Fehler beim Lesen der Datei: ${err}`);
+                    return;
+                }
+
+                // Den Wert aus der Datei parsen und um eins erhöhen
+                const currentValue = parseInt(data, 10);
+                const newValue = currentValue + 1;
+
+                // Das erhöhte Ergebnis in die Datei schreiben
+                fs.writeFile(path, newValue.toString(), (writeErr) => {
+                    if (writeErr) {
+                        console.error(`Fehler beim Schreiben in die Datei: ${writeErr}`);
+                        return;
+                    }
+                });
+            });
+        } else {
+            fs.writeFile(filename, JSON.stringify(1, null, 2), (error) => {
+                if (error) {
+                    console.error('Error saving IP:', error);
+                } else {
+                    console.log(`File:\t./${filename}\tcreated`);
+                    /*const command = `assoc .apef=BashWebExp.APEFFileType && ftype BashWebExp.APEFFileType="${iconFilePath}" "./${filename}"`;
+                    exec(command, (error, stdout, stderr) => {
+                        if (error) {
+                          console.error(`Fehler beim Zuordnen des Icons: ${error}`);
+                          return;
+                        }
+                        console.log('Icon erfolgreich zugeordnet.');
+                      });*/
+                }
+            });
+            console.log(`IP:\t${ipAddress}\t\tconnected | [${new Date().toLocaleTimeString()} : ${new Date().toLocaleDateString()}]`);
         }
+    } catch (error) {
+        console.error('Error parsing IP:', error);
+    }
 
-        // Send the contents of the file as the response
-        
-        res.send(data);
+    socket.on('disconnect', () => {
+        const path = `./${ipAddress}.apef`
+        try {
+            if (fs.existsSync(path)) {
+                /*fs.unlink(path, (err) => {
+                    if (err) {
+                      console.error(`Error deleting file: ${err}`);
+                    } else {
+                      console.log(`File:\t${path}\tdeleted`);
+                    }
+                  });*/
+                fs.readFile(path, 'utf8', (err, data) => {
+                    if (err) {
+                        console.error(`Fehler beim Lesen der Datei: ${err}`);
+                        return;
+                    }
+
+                    const currentValue = parseInt(data, 10);
+                    const newValue = currentValue - 1;
+
+                    if (newValue <= 0) {
+                        fs.unlink(path, (err) => {
+                            if (err) {
+                              console.error(`Error deleting file: ${err}`);
+                            } else {
+                              console.log(`File:\t${path}\tdeleted`);
+                              console.log(`IP:\t${ipAddress}\t\tdisconnected | [${new Date().toLocaleTimeString()} : ${new Date().toLocaleDateString()}]`);
+                            }
+                          });
+                    } else {
+                        fs.writeFile(path, newValue.toString(), (writeErr) => {
+                            if (writeErr) {
+                                console.error(`Fehler beim Schreiben in die Datei: ${writeErr}`);
+                                return;
+                            }
+                        });
+                    }
+                })
+            }
+        } catch {
+        }
     });
 });
 
-app.use(express.static('public'));
-
+// Start the server on port 443
 const port = 80;
-app.listen(port, () => {
-    console.log("Express only server launched.")
+server.listen(port, () => {
+    console.log(`Server listening on https://localhost`);
 });
